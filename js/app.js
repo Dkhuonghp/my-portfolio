8ef2e680fc2323998f9c4747d7939df3da1bd065
Dkhuonghp <khuonghp97@gmail.com>
2023-02-18 19:48:18 +0900

    modified

--- js/app.js
+++ js/app.js
@@ -1,43 +1,6 @@
 const works = document.querySelector('.works');
+import args from "./data"
 
-const args = [
-    {
-        name: 'IT19',
-        img: '/img/works/work_1.svg',
-        weblink:'https://it19.vercel.app/',
-        github:'https://github.com/Dkhuonghp/it_19'
-    },
-    {
-        name: 'Moment Memorie',
-        img: '/img/works/work_2.svg',
-        weblink:'http://click.ecc.ac.jp/ecc/letanloc/Portfolio/work/Project_01/MomentMemorie/',
-        github:'https://github.com/Dkhuonghp/MomentMemorie',
-    },
-    {
-        name: 'Job World',
-        img: '/img/works/work_3.png',
-        weblink:'https://click.ecc.ac.jp/ecc/ndkhuong/Job_World/',
-        github:'https://github.com/Dkhuonghp/Job_World'
-    },
-    {
-        name: 'Notes',
-        img: 'https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/f0/e0/7e/f0e07e01-0a36-0ee3-edea-5f0e28cac5db/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x600wa.png',
-        weblink:'https://note-app-dkhuonghp.vercel.app/',
-        github:'https://github.com/Dkhuonghp/note-app',
-    },
-    {
-        name: 'Travel',
-        img: '/img/works/work_8.png',
-        weblink:'https://friendly-beijinho-f2679f.netlify.app/',
-        github:'https://github.com/Dkhuonghp/Travel',
-    },
-    {
-        name: 'Portfolio',
-        img: '/img/logo.svg',
-        weblink:'https://portfolio-ndkhuong.netlify.app/',
-        github:'https://github.com/Dkhuonghp/my-portfolio',
-    },
-]
 //! Works
 const html = args.map(item => {
     return `
@@ -59,7 +22,7 @@ const html = args.map(item => {
                         </a>
                     </div>
                 </div>
-                <div class='app__work-content app__flex'>
+                <div class='zen-maru app__work-content app__flex'>
                     <h4>
                         ${item.name}
                     </h4>
---
+++ js/data.js
@@ -0,0 +1,40 @@
+const args = [
+    {
+        name: 'IT19',
+        img: '/img/works/work_1.svg',
+        weblink:'https://it19.vercel.app/',
+        github:'https://github.com/Dkhuonghp/it_19'
+    },
+    {
+        name: 'Moment Memorie',
+        img: '/img/works/work_2.svg',
+        weblink:'http://click.ecc.ac.jp/ecc/letanloc/Portfolio/work/Project_01/MomentMemorie/',
+        github:'https://github.com/Dkhuonghp/MomentMemorie',
+    },
+    {
+        name: 'Job World',
+        img: '/img/works/work_3.png',
+        weblink:'https://click.ecc.ac.jp/ecc/ndkhuong/Job_World/',
+        github:'https://github.com/Dkhuonghp/Job_World'
+    },
+    {
+        name: 'Notes',
+        img: 'https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/f0/e0/7e/f0e07e01-0a36-0ee3-edea-5f0e28cac5db/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x600wa.png',
+        weblink:'https://note-app-dkhuonghp.vercel.app/',
+        github:'https://github.com/Dkhuonghp/note-app',
+    },
+    {
+        name: 'Travel',
+        img: '/img/works/work_8.png',
+        weblink:'https://friendly-beijinho-f2679f.netlify.app/',
+        github:'https://github.com/Dkhuonghp/Travel',
+    },
+    {
+        name: 'Portfolio',
+        img: '/img/logo.svg',
+        weblink:'https://portfolio-ndkhuong.netlify.app/',
+        github:'https://github.com/Dkhuonghp/my-portfolio',
+    },
+]
+
+export default args
